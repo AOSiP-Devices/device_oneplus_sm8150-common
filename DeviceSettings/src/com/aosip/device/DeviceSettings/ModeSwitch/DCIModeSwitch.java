694923a705e7607b8ce2b6770b46075ec6969d7d
Daniel Micay <danielmicay@gmail.com>
2018-01-28 23:16:37 +0100

    sm8150-common:enable Verizon visual voicemail support
    Change-Id: I2dc668f790b626793b459139c1ed0ff76151c8ea
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
+++ DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
@@ -24,7 +24,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceManager;
 
-import com.android.internal.util.aosip.FileFileUtils;
+import com.android.internal.util.aosip.FileUtils;
 
 public class DCIModeSwitch implements OnPreferenceChangeListener {
 
