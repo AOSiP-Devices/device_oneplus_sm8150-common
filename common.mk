aba49710e53290e705b1113f2ce83ec617714050
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-06-20 20:01:11 -0400

    sm8150-common: AOSiP-ify
    Change-Id: Ibc0c1f3500c4777740afc64c64b26f0cdf9e3f85
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- common.mk
+++ common.mk
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
