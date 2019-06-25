43a2382fbe23de59cea650acbfd320bb37f7e1f6
Peter Cai <peter@typeblog.net>
2019-06-25 19:17:37 +0800

    sm8150-common: import sensors multihal
    * with a full vendor image, we can modify the sensors hal as we want. We
    can replace the reported sensor type directly from HAL, instead of
    relying on a framework patch.
    
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- common.mk
+++ common.mk
@@ -337,8 +337,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-	android.hardware.sensors@1.0-impl \
-	android.hardware.sensors@1.0-service \
+	android.hardware.sensors@1.0-impl.oneplus_msmnile \
+	android.hardware.sensors@1.0-service.oneplus_msmnile \
 	libsensorndkbridge
 
 # Sensor HAL conf file
