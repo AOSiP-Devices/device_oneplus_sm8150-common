355378cde0cd8134d125e7ab3cb22ffb8c5765de
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-07-20 19:33:47 -0400

    sm8150-common: DisplayModes: Drop "loading effect mode"
    Change-Id: I39aa66cb58a828d7905ef56e8f0c2502b117e1b3
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- DisplayMode/src/org/lineageos/settings/DisplayMode/PanelSettings.java
+++ DisplayMode/src/org/lineageos/settings/DisplayMode/PanelSettings.java
@@ -57,8 +57,6 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             checkedButtonId = R.id.dci_mode;
         } else if (SRGBModeSwitch.isCurrentlyEnabled(getContext())) {
             checkedButtonId = R.id.srgb_mode;
-        } else if (LoadingEffectModeSwitch.isCurrentlyEnabled(getContext())) {
-            checkedButtonId = R.id.loading_effect_mode;
         } else if (WideColorModeSwitch.isCurrentlyEnabled(getContext())) {
             checkedButtonId = R.id.wide_color_mode;
         }
@@ -85,8 +83,6 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
             Utils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(LoadingEffectModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_LOADING_SWITCH, false);
             Utils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
             Utils.writeValue(SRGBModeSwitch.getFile(), "1");
@@ -96,8 +92,6 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
             Utils.writeValue(NightModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(LoadingEffectModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_LOADING_SWITCH, false);
             Utils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
             Utils.writeValue(DCIModeSwitch.getFile(), "1");
@@ -107,8 +101,6 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
             Utils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(LoadingEffectModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_LOADING_SWITCH, false);
             Utils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
             Utils.writeValue(NightModeSwitch.getFile(), "1");
@@ -120,21 +112,8 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
             Utils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(LoadingEffectModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_LOADING_SWITCH, false);
             Utils.writeValue(WideColorModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-        } else if (checkedId == R.id.loading_effect_mode) {
-            Utils.writeValue(DCIModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
-            Utils.writeValue(NightModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
-            Utils.writeValue(SRGBModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(WideColorModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
-            Utils.writeValue(LoadingEffectModeSwitch.getFile(), "1");
-            edit.putBoolean(DeviceSettings.KEY_LOADING_SWITCH, true);
         } else if (checkedId == R.id.wide_color_mode) {
             Utils.writeValue(DCIModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
@@ -142,8 +121,6 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             edit.putBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
             Utils.writeValue(SRGBModeSwitch.getFile(), "0");
             edit.putBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
-            Utils.writeValue(LoadingEffectModeSwitch.getFile(), "0");
-            edit.putBoolean(DeviceSettings.KEY_LOADING_SWITCH, false);
             Utils.writeValue(WideColorModeSwitch.getFile(), "1");
             edit.putBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, true);
         }
