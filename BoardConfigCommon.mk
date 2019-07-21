8423403c4c4e48f90e3a8237a505d4ec1e5fc92a
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-07-21 06:37:27 -0400

    sm8150-common: Remove extra AVB args
    having this kills verifying zip for OTA
    
    Change-Id: I65ca31b1dd95e87ddc773ae53b334ba6fce4def2
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,7 +180,6 @@ PRODUCT_SOONG_NAMESPACES += $(VENDOR_PATH)
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # WiFi
