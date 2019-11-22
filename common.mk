1e60ceb1a862e36f3091f263198d9c72986cd315
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-11-22 21:42:59 +0000

    sm8150-common: Initial AOSiP Bringup
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- common.mk
+++ common.mk
@@ -19,12 +19,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true
