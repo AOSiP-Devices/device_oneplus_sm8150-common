PeterCxy/android_device_oneplus_sm8150-common

sm8150-common: Exclude Snap overlays from RRO
Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

common.mk
@@ -22,7 +22,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true

AOSiP-Devices/device_oneplus_sm8150-common

Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

common.mk
@@ -22,6 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-aosip/packages/apps/Snap
 
 # A/B
 AB_OTA_UPDATER := true