be755ed06b70554e8209a7fe02f06e7918e93c23
Omkar Chandorkar <forumomkar@gmail.com>
2020-10-15 08:40:27 +0530

    sm8150-common: init: handle dalvik props based on sysram
    Change-Id: I7d7202a0ae22428f12a5e0bd7acf3830ea5f44e7
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- common.mk
+++ common.mk
@@ -220,9 +220,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0.vendor
 
-# Dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
