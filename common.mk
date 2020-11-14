1d7a434f54cdc175ad3fb7c416949e7cd480fc8a
Omkar Chandorkar <gotenksIN@aosip.dev>
2020-11-14 10:52:00 +0530

    sm8150-common: blob librmnetctl
    Q netmgrd isn't a fan of R librmnetctl
    ship q prebuilt so RIL won't die
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I7fff8cc6d29b3b7ba9bb2d38e438c03eaf022ba1

--- common.mk
+++ common.mk
@@ -402,7 +402,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
-    librmnetctl \
     libxml2
 
 # Vendor libstdc++
